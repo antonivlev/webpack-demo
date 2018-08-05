028e6a36db608f8d53efa5e50b2c54b6484667cd
anton <anton.ivlev97@gmail.com>
2018-08-05 16:29:37 +0100

    initial webpack fun

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+console.log('webpack config executed, path path:');
+console.log(path.resolve(__dirname, 'dist'));
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
